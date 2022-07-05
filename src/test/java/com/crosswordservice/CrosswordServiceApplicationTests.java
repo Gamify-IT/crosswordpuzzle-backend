46b26080705b7f1c5a525ea8bec9262f5adb7479
Gilian Rehm <g_rehm@ich-habe-fertig.com>
2022-07-05 19:44:46 +0200

    add first test

--- src/test/java/com/crosswordservice/CrosswordServiceApplicationTests.java
+++ src/test/java/com/crosswordservice/CrosswordServiceApplicationTests.java
@@ -1,8 +1,20 @@
 package com.crosswordservice;
 
+import com.crosswordservice.baseClasses.Configuration;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CrosswordServiceApplicationTests {
 
+    @Test
+    void testStartApplication() {
+        Configuration test = new Configuration();
+        String testName = "testName";
+
+        test.setName(testName);
+        assertThat(test.getName()).isEqualTo(testName);
+    }
 }
